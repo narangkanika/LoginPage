c7bbbcd017bae4644ca9004317c98f73250ed75e
Kanika Narang <kanikanarang0410@gmail.com>
2017-01-06 23:04:43 +0530

    changed controller

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -1,15 +1,16 @@
 (function(){
 
-  var loginController = function($scope,$log,loginfactory){
+  var loginController = function($scope,$log,$window,loginfactory){
     $scope.username="kanika ";
     $scope.password=" kanika";
-    $scope.success = function() {
-       $log.log("hii kanika");
+    $scope.success = function(password) {
+       $window.alert("hii kanika");
     };
 
   }
-}());
-loginController.$inject = ['$scope', '$log','loginFactory'];
+
+loginController.$inject = ['$scope', '$log','$window','loginFactory'];
 
 angular.module('login')
   .controller('loginController', loginController);
+}());
